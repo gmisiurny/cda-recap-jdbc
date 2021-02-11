035987f7d89e6d5c7d37537c3186e4253f304b13
Pierre-Yves.AUGEM <pierreyves.augem@gmail.com>
2021-02-11 12:01:07 +0100

    creation classe Category

--- src/com/cda/jdbc/dao/DAO.java
+++ src/com/cda/jdbc/dao/DAO.java
@@ -5,6 +5,6 @@ import java.util.List;
 public interface DAO<T> {
 	T save(T o);
 	List<T> getAll();
-	void remove(T primaryKey);
-	void update(T primaryKey);
+	void remove(String nomOuID);
+	void update(String nomOuID);
 }
---
+++ src/com/cda/jdbc/data/Category.java
@@ -0,0 +1,34 @@
+package com.cda.jdbc.data;
+
+import java.util.List;
+
+import com.cda.jdbc.dao.DAO;
+
+public class Category implements DAO<Category>{
+
+
+	@Override
+	public Category save(Category o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(String nomOuID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(String nomOuID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Category> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
